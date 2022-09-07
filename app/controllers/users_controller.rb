46ef5641436b98f90f77bd30c2c94cb767f49143
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2022-09-07 16:58:49 +0900

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      # 
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
     else
       render :new
     end
