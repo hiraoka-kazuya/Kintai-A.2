4728d7ee5a66ea4187d54ce74653732b5c2cae4a
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2022-09-27 11:05:25 +0900

    勤怠編集画面の入力フォーム機能　作成

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,7 @@ class AttendancesController < ApplicationController
   before_action :set_user, only: :edit_one_month
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
-  
+
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
@@ -26,6 +26,9 @@ class AttendancesController < ApplicationController
   end
 
   def edit_one_month
+  end
 
+  def update_one_month
+    
   end
 end
