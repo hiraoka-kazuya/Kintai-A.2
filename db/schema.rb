441781b7e063ec1bd3c322a0d1c2be4b55b0be57
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2022-10-06 12:04:53 +0900

    追加カラムのmigrate完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221004004458) do
+ActiveRecord::Schema.define(version: 20221006030026) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 20221004004458) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "id"
-    t.integer "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
@@ -43,8 +41,9 @@ ActiveRecord::Schema.define(version: 20221004004458) do
     t.string "department"
     t.datetime "basic_time", default: "2022-10-03 23:00:00"
     t.datetime "work_time", default: "2022-10-03 22:30:00"
-    t.datetime "started_at", default: "2022-10-03 22:30:00"
-    t.datetime "finished_at", default: "2022-10-03 22:30:00"
+    t.time "designated_work_start_time"
+    t.time "designated_work_end_time"
+    t.integer "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
