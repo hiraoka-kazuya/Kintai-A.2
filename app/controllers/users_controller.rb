43148816a7fd4a06653b3ef50e2f80d317529f3c
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2022-12-01 22:42:30 +0900

    　業申請

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :list_of_employees]
   before_action :logged_in_user, only: [:edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info, :employee_at_work_index]
+  before_action :correct_user, only: [:edit, :update, :edit_basic_info, :update_basic_info, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info, :employee_at_work_index, :list_of_employees]
   before_action :set_one_month, only: :show
+  before_action :not_allow_admin_user, only: :show
 
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
